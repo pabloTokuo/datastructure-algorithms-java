bb76a6b678a8137c9bfda729f0de29d2618aa27c
Pablo Tokuo <pbo_1996@hotmail.com>
2022-12-01 11:15:19 -0300

    Verificar tamanho e imprimir elementos do vetor

--- src/application/Program4.java
+++ src/application/Program4.java
@@ -2,16 +2,18 @@ package application;
 
 import datastructure.vector.Vector;
 
-public class Program3 {
+public class Program4 {
     public static void main(String[] args) {
 
-        Vector vect = new Vector(2);
+        Vector vect = new Vector(10);
 
         vect.adiciona("elemento 1");
         vect.adiciona("elemento 2");
         vect.adiciona("elemento 3");
 
+        System.out.println(vect.tamanho());
         System.out.println(vect);
+
 
     }
 }
